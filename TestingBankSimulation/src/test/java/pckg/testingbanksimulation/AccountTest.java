d5cec585b1f793112892d58f544dae983e448613
Reynaldo Marin <reynaldo.hernandez00@gmail.com>
2020-04-21 05:23:07 -0400

    Added initial AccountTest.java

---
+++ TestingBankSimulation/src/test/java/pckg/testingbanksimulation/AccountTest.java
@@ -0,0 +1,281 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pckg.testingbanksimulation;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author reyna
+ */
+public class AccountTest {
+    
+    public AccountTest() {
+    }
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of deposit method, of class Account.
+     */
+    @org.junit.jupiter.api.Test
+    public void testDeposit() {
+        System.out.println("deposit");
+        double amount = 0.0;
+        Account instance = null;
+        instance.deposit(amount);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of withdraw method, of class Account.
+     */
+    @org.junit.jupiter.api.Test
+    public void testWithdraw() {
+        System.out.println("withdraw");
+        double amount = 0.0;
+        Account instance = null;
+        instance.withdraw(amount);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of transfer method, of class Account.
+     */
+    @org.junit.jupiter.api.Test
+    public void testTransfer() {
+        System.out.println("transfer");
+        Account fromAccount = null;
+        Account toAccount = null;
+        double amount = 0.0;
+        Account.transfer(fromAccount, toAccount, amount);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getAccountDescription method, of class Account.
+     */
+    @org.junit.jupiter.api.Test
+    public void testGetAccountDescription() {
+        System.out.println("getAccountDescription");
+        Account instance = null;
+        String expResult = "";
+        String result = instance.getAccountDescription();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setAccountDescription method, of class Account.
+     */
+    @org.junit.jupiter.api.Test
+    public void testSetAccountDescription() {
+        System.out.println("setAccountDescription");
+        String accountDescription = "";
+        Account instance = null;
+        instance.setAccountDescription(accountDescription);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getBalance method, of class Account.
+     */
+    @org.junit.jupiter.api.Test
+    public void testGetBalance() {
+        System.out.println("getBalance");
+        Account instance = null;
+        double expResult = 0.0;
+        double result = instance.getBalance();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getTransactions method, of class Account.
+     */
+    @org.junit.jupiter.api.Test
+    public void testGetTransactions() {
+        System.out.println("getTransactions");
+        Account instance = null;
+        List<Transaction> expResult = null;
+        List<Transaction> result = instance.getTransactions();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getTransaction method, of class Account.
+     */
+    @org.junit.jupiter.api.Test
+    public void testGetTransaction() {
+        System.out.println("getTransaction");
+        int transactionId = 0;
+        Account instance = null;
+        Transaction expResult = null;
+        Transaction result = instance.getTransaction(transactionId);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getAccountId method, of class Account.
+     */
+    @org.junit.jupiter.api.Test
+    public void testGetAccountId() {
+        System.out.println("getAccountId");
+        Account instance = null;
+        String expResult = "";
+        String result = instance.getAccountId();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getCustomerId method, of class Account.
+     */
+    @org.junit.jupiter.api.Test
+    public void testGetCustomerId() {
+        System.out.println("getCustomerId");
+        Account instance = null;
+        String expResult = "";
+        String result = instance.getCustomerId();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getAccountCreationDate method, of class Account.
+     */
+    @org.junit.jupiter.api.Test
+    public void testGetAccountCreationDate() {
+        System.out.println("getAccountCreationDate");
+        Account instance = null;
+        LocalDateTime expResult = null;
+        LocalDateTime result = instance.getAccountCreationDate();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of hashCode method, of class Account.
+     */
+    @org.junit.jupiter.api.Test
+    public void testHashCode() {
+        System.out.println("hashCode");
+        Account instance = null;
+        int expResult = 0;
+        int result = instance.hashCode();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of equals method, of class Account.
+     */
+    @org.junit.jupiter.api.Test
+    public void testEquals() {
+        System.out.println("equals");
+        Object obj = null;
+        Account instance = null;
+        boolean expResult = false;
+        boolean result = instance.equals(obj);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of compareTo method, of class Account.
+     */
+    @org.junit.jupiter.api.Test
+    public void testCompareTo() {
+        System.out.println("compareTo");
+        Account other = null;
+        Account instance = null;
+        int expResult = 0;
+        int result = instance.compareTo(other);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toString method, of class Account.
+     */
+    @org.junit.jupiter.api.Test
+    public void testToString() {
+        System.out.println("toString");
+        Account instance = null;
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    public class AccountImpl extends Account {
+
+        public AccountImpl() {
+            super(null, 0.0, "");
+        }
+
+        public void deposit(double amount) {
+        }
+
+        public void withdraw(double amount) {
+        }
+    }
+    
+}
